2d3a2db8f12e435954892cfd9f38a260a5a7a9c5
Bryon Williams <bryon.williams@chromehearts.com>
2026-09-04 10:24:35 -0700

    Remove legacy plugins from mcc roster

--- tools/mcc.py
+++ tools/mcc.py
@@ -24,7 +24,7 @@ MCC_VERSION = "1.29.0"
 import json
 import time
 
-PLUGINS = ("pdt", "mam", "mama", "bus", "docs", "plumb")
+PLUGINS = ("pdt", "mama", "docs", "plumb")
 MARKETPLACE = "methodical-cc"
 STATE_DIR_GLOBS = (".mcc", ".mcc-*")
 
